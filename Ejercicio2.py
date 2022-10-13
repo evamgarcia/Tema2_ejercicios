b2fde523b21199e0a0d791221ccd9439849929ef
evamgarcia <emgarcia2911@gmail.com>
2022-10-13 13:41:36 +0200

    ejercicios

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+#Transforma este texto:
+#un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro #monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro
+#En este otro:
+#Un día que el viento soplaba con fuerza...
+#- Mira como se mueve aquella banderola -dijo un monje.
+#- Lo que se mueve es el viento -respondió otro monje.
+#- Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.
+#Lo único prohibido es modificar directamente el texto.
+
+texto = 'un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro'
+print(texto.split('#'))
